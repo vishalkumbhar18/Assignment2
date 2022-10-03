75153fced5067b061c6a9c5bcd6e9c8e9297c3d3
Vishal Kumbhar <86111799+vishalkumbhar18@users.noreply.github.com>
2022-10-03 21:32:48 +0530

    Add files via upload

---
+++ Assignment2/A2_1.py
@@ -0,0 +1,45 @@
+#1: #this is how we add comment by using "#"
+
+
+
+print("Learning Python")
+print()
+
+"""
+this is how we #2
+add multine line 
+comment bye Using 
+
+"""
+a=1
+b=2
+c=3
+d=4
+print(a,b,c,d,sep="\n") 
+print()
+
+#3
+g=35
+h=True
+i="MysirG"
+j=5.46
+k=3+4j
+print(type(g),type(h),type(i),type(j),type(k),sep="\n")
+print()
+
+#4
+o=5
+p=5
+print(id(o))
+print(id(p))
+print()
+
+#5
+x=False
+y="Naman"
+z=1+2J
+v=21
+print(x,type(x),id(x))
+print(y,type(y),id(y))
+print(z,type(z),id(z))
+print(v,type(v),id(v))
---
+++ Assignment2/A2_2.py
@@ -0,0 +1,29 @@
+#6
+from ast import keyword
+from datetime import datetime
+from keyword import kwlist
+from time import time
+
+
+print(kwlist)
+print()
+#7
+# help()>keywords
+#8importing variable from A2_1.py
+from A2_1 import a 
+print(a)
+print()
+
+
+
+#9 
+# keywords used as data in Python script>True,False,None
+
+#10
+t=datetime.now
+d=t.strftime("%B %d %Y")
+e=t.strftime("%d %b %Y")
+print(t)
+print(d)
+print(e)
+
